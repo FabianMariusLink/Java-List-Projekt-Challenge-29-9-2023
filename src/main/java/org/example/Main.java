8e9a301f41d3f4daf595bdef85a296c3f15a7594
FabianMariusLink <fabian.marius.link@gmail.com>
2023-09-29 12:16:56 +0200

    add method printTotalStudents to class School change size to toString

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,6 +15,19 @@ public class Main {
         students.add(new Student("Antonia", "Ott", "ted54572"));
         students.add(new Student("Medea", "Ernst", "rt546332"));
 
+        Student student11 = new Student("Fabian", "Link", "tuf45372");
+        Student student12 = new Student("Antonia", "Ott", "ted54572");
+        Student student13 = new Student("Medea", "Ernst", "rt546332");
+
+
+
+        School.addStudent(school1, student11);
+        School.addStudent(school1, student12);
+        School.addStudent(school1, student13);
+
+        school1.printTotalStudents();
+
+
         for (Student s : students){
             System.out.println(s.getFirstName());
         }
