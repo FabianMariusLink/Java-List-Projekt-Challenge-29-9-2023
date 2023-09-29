ca0ba8f2cca6d78d83d86728967656afc54e7ba1
FabianMariusLink <fabian.marius.link@gmail.com>
2023-09-29 11:02:17 +0200

    add class School

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import org.example.school.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Student> students = new ArrayList<>();
+        //System.out.println("Hello world!");
+
+        students.add(new Student("Fabian", "Link", "tuf45372"));
+        students.add(new Student("Antonia", "Ott", "ted54572"));
+        students.add(new Student("Medea", "Ernst", "rt546332"));
+
+        for (Student s : students){
+            System.out.println(s.getFirstName());
+        }
     }
 }
